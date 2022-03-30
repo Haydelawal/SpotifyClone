881737939aeb70a28ee97762f70367aa7343c437
LAWAL <haydelawal@gmail.com>
2022-03-30 22:29:21 +0100

    Spotify Clone with cloud storage

--- app/src/main/java/com/plcoding/spotifycloneyt/MainActivity.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/MainActivity.kt
@@ -2,10 +2,24 @@ package com.plcoding.spotifycloneyt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.RequestManager
+import com.plcoding.spotifycloneyt.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+
+    @Inject
+    lateinit var glide: RequestManager
+
+    //activity_main ==>> Activity_Main_Binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
