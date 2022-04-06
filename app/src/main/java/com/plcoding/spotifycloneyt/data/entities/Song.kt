4e900964eb3fba6b47df4b0331b75b87a2bffc93
LAWAL <haydelawal@gmail.com>
2022-04-06 16:38:08 +0100

    Spotify Clone

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/data/entities/Song.kt
@@ -0,0 +1,11 @@
+package com.plcoding.spotifycloneyt.data.entities
+
+data class Song (
+
+    val mediaId: String ="",
+    val songUrl: String ="",
+    val imageUrl: String ="",
+    val subtitle: String ="",
+    val title: String =""
+
+        )
